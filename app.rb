6ea7f26e321b447b344a7c6089b77dc6af50ae62
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-29 17:12:19 +0100

    created attack button for player 1 to attack player 2

--- app.rb
+++ app.rb
@@ -21,6 +21,12 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
+  get '/attack' do
+    @Player1_name = session[:Player1_name]
+    @Player2_name = session[:Player2_name]
+    erb(:attack)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,7 +1,7 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
     sign_in_and_play
-    expect(page).to have_content 'Steve: 8'
+    expect(page).to have_content 'Steve: HP8'
   end
 end
 
