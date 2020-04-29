24b606fe169c46100a8cf4514097e591811a1b3c
tracht <marija.trachtenberg@gmail.com>
2020-04-29 15:49:42 +0100

    form now shows player 2 label

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,7 @@
 # notice we didn't need to require app file, as it is in the spec_helper
 
 # write a feature test that checks that the homepage says Testing infra working!
+
 # feature 'Testing infrastructure' do
 #  scenario 'Can run app and check page content' do
 #    visit('/')
