3828e07f8918052cc91f75984997b9e7c34e16a6
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-01 14:33:09 +0100

    added player class and test

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require 'player'
+
+describe Player do
+  subject(:john) { Player.new("John") }
+  describe '#name' do
+    it 'shows the name of the player' do
+      expect(john.name).to eq 'John'
+    end
+  end
+end
